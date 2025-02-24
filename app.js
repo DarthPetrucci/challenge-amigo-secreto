bf1e4e933ab03a0b3d65107c0647637db9ea7f66
Diego_Amezquita <diegof.amezquita@gmail.com>
2025-02-24 14:21:07 -0500

    Se agregó la función que muestra y actualiza la lista de nombres agregados

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ let amigos = [];
 /*Esta función agrega un amigo a la lista de amigos*/
 function agregarAmigo() {
     
-    amigo_added = document.getElementById("amigo").value; /*Obtiene el nombre del amigo ingresado por el usuario*/
+    let amigo_added = document.getElementById("amigo").value; /*Obtiene el nombre del amigo ingresado por el usuario*/
 
     if(amigo_added != "") {
         amigos.push(amigo_added); /*Agrega el amigo a la lista de amigos*/
@@ -20,4 +20,21 @@ function agregarAmigo() {
         alert("Por favor, ingresa un nombre de amigo válido."); 
         /*Muestra un mensaje de alerta al usuario indicando que debe ingresar un nombre de amigo*/
     }
+
+    listarAmigos();
+}
+
+function listarAmigos() {
+
+    const lista = document.getElementById("listaAmigos"); /*Obtiene la lista de amigos*/
+
+    lista.innerHTML = ""; /*Limpia la lista de amigos*/
+
+    for (let i = 0; i < amigos.length; i++) {
+
+        const NuevoElemento = document.createElement("li"); /*Crea un nuevo elemento de lista*/
+        NuevoElemento.textContent = amigos[i]; /*Asigna el nombre de amigo al nuevo elemento de lista*/
+        lista.appendChild(NuevoElemento); /*Agrega el nuevo elemento de lista a la lista de amigos*/
+    }
+
 }
