e6bc119e3cfaf25b33792747ba52211da5d4dd8a
Diego_Amezquita <diegof.amezquita@gmail.com>
2025-02-24 11:58:36 -0500

    Se creó un array en donde se guarda la lista de amigos

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+/* Crea una lista vacía para almacenar los nombres de tus amigos */
+
+let amigos = [];
+
